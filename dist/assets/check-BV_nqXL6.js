9ca6c5379724d176183605da5df5b05aa2981817
emmeartson <emmeartson@gmail.com>
2026-07-07 20:11:55 +0600

    updated dist

--- dist/assets/check-BV_nqXL6.js
+++
@@ -1,1 +0,0 @@
-import{u as c}from"./index-COge0k8u.js";const o=c("Check",[["path",{d:"M20 6 9 17l-5-5",key:"1gmf2c"}]]);export{o as C};
---
+++ dist/assets/chevron-left-ytoYPscT.js
@@ -0,0 +1,1 @@
+import{u as e}from"./index-NigPdmqO.js";const t=e("ChevronLeft",[["path",{d:"m15 18-6-6 6-6",key:"1wnfg3"}]]);export{t as C};
